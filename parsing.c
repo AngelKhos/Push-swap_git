f2c428592b9377d84fbccc8773465eab4073337d
Aurore-Alexandra Thomas <authomas@z4r12p5.42lyon.fr>
2025-01-31 23:11:23 +0100

    push

--- parsing.c
+++ parsing.c
@@ -1,43 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   swap.c                                             :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: authomas <authomas@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/29 08:53:43 by authomas          #+#    #+#             */
-/*   Updated: 2025/01/31 18:29:21 by authomas         ###   ########lyon.fr   */
+/*   Created: 2025/01/31 18:08:29 by authomas          #+#    #+#             */
+/*   Updated: 2025/01/31 18:08:42 by authomas         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	swap(t_stack *any)
-{
-	int	buff;
-
-	if (any == NULL || any->next == NULL)
-		return ;
-	buff = any->content;
-	any->content = any->next->content;
-	any->next->content = buff;
-}
-
-void	swap_a(t_stack *a)
-{
-	swap(a);
-	ft_printf("sa\n");
-}
-
-void	swap_b(t_stack *b)
-{
-	swap(b);
-	ft_printf("sb\n");
-}
-
-void	ss(t_stack *a, t_stack *b)
-{
-	swap(a);
-	swap(b);
-	ft_printf("ss\n");
-}
